0b6939374580468a78fd8b8fb565b60853874073
john-kloss <jonathan.kloss1@gmail.com>
2016-10-25 13:26:59 +0200

    complete task1

--- Praxisaufgaben/p01_image_statistics.m
+++ Praxisaufgaben/p01_image_statistics.m
@@ -1,8 +1,7 @@
-function [greyMed, numOfPix, expec, var] = p01_image_statistics(image)
+function [greyMed, numOfPix, expec, varianz] = p01_image_statistics(image)
   greyMed = median(median(image));
   numOfPixAll = size(image,1) * size(image,2) %numOfPix in x * numOfPix in y direction
-  numOfPixHigh = size(image,1)*size(image,2)
-  numOfPix = numOfPixAll - size(image>100,1)*size(image>100,2) -  size(image<50,1)*size(image<50,2)
-  
-  var = 0;
+  numOfPix = numOfPixAll - sum(sum(image < 50)) - sum(sum(image > 150));
+  expec = sum(image(:))/numOfPixAll;
+  varianz = var(image(:));
 end
